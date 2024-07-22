633f32fbb44b7d496baa5467804e123a6a843531
ShakedDahari <shaked1299@gmail.com>
2024-07-22 13:19:08 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
-server.use('/api/codeBlocks', require('./Routes/codeBlocksRoute'));
+server.use('/api/codeBlocks', require('./routes/codeBlocksRoute'));
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
